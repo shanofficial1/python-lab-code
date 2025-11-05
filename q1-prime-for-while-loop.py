721dd054393f0151fa0e0bd70c20984382a4ce63
Shan20122003 <shanr1164@gmail.com>
2025-11-05 17:27:09 +0530

    while added

--- q1-prime-for-while-loop.py
+++ q1-prime-for-while-loop.py
@@ -10,3 +10,18 @@ if n>=2:
 else:
     print("No prime numbers")
 
+
+
+while True:
+    if n>=2:
+        for i in range(2,n+1):
+            count=0
+            for j in range(1,i+1):
+                if i%j==0:
+                    count+=1
+            if count==2:
+                print(i)
+
+    else:
+        print("No prime numbers")
+    break
