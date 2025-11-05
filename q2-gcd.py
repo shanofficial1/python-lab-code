ad6544633e168ac8f4f7a2486b2f1626c36daa2c
Shan20122003 <shanr1164@gmail.com>
2025-11-05 17:34:30 +0530

    q2

--- q2-gcd.py
+++ q2-gcd.py
@@ -1,3 +1,13 @@
+# Repeat the following steps until b becomes 0:
+
+# Set temp = b
+
+# Set b = a % b (remainder when a is divided by b)
+
+# Set a = temp
+
+# When b becomes 0, the current value of a is the GCD.
+
 def gcd(a,b):
     if b==0:
         return a
