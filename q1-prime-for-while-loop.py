cdc28300264b7797f7fb52b1b96aa5f079b15b8c
Shan A <shanr1164@gmail.com>
2025-11-21 21:08:36 +0530

    Simplify prime number detection logic
    Refactored prime number calculation using a while loop.

--- q1-prime-for-while-loop.py
+++ q1-prime-for-while-loop.py
@@ -1,27 +1,13 @@
-n=int(input("Enter a number: "))
-if n>=2:
-    for i in range(2,n+1):
-        count=0
-        for j in range(1,i+1):
-            if i%j==0:
-                count=count+1
-        if count==2:
-            print(i)
-else:
-    print("No prime numbers")
-
-
-
-while True:
-    if n>=2:
-        for i in range(2,n+1):
-            count=0
-            for j in range(1,i+1):
-                if i%j==0:
-                    count+=1
-            if count==2:
-                print(i)
-
-    else:
-        print("No prime numbers")
-    break
+n=int(input(("Enter the limit")))
+i=1
+while(i<=n):
+    count=0
+    for j in range(1,i+1):
+        if i%j==0:
+            count+=1
+            if count>2:
+                break
+           
+    if count==2:
+        print(i)
+    i+=1
