000672a9725b50d342685044468173090e4f9777
Shan20122003 <shanr1164@gmail.com>
2025-12-03 08:56:03 +0530

    4 question

---
+++ 4.py
@@ -0,0 +1,19 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+
+print("Prime numbers between", start, "and", end, "are:")
+
+for n in range(start, end + 1):
+    if is_prime(n):
+        print(n, end=" ")
