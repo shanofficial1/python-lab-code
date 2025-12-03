45d7b883a210ea7693d02a11fd20cf53d810e380
Shan20122003 <shanr1164@gmail.com>
2025-12-03 09:03:40 +0530

    Added sum of three numbers program

---
+++ sumofthree.py
@@ -0,0 +1,7 @@
+import sum
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=int(input("Enter the thrid number"))
+ans=sum.sum(a,b,c)
+print("Summ of three number is",ans)
+    
