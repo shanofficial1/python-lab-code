2e83de19396b8535af1da284dd090cc3af3b6505
Shan20122003 <shanr1164@gmail.com>
2025-12-03 09:03:39 +0530

    Added check program

---
+++ check.py
@@ -0,0 +1,7 @@
+def check_execution():
+    if __name__ == "__main__":
+        print("The program is executed directly (main program).")
+    else:
+        print("The program is imported as a module.")
+
+check_execution()
